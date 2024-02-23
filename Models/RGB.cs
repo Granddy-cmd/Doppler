a403c8e0bc3854827100341e1119f69c44018cbb
maria <maria.ryasna@gmail.com>
2024-02-24 01:26:53 +0200

    test

---
+++ Models/RGB.cs
@@ -0,0 +1,18 @@
+﻿namespace Doppler.Models
+{
+    public class RGB
+    {
+        public int Red { get; init; }
+
+        public int Green { get; init; }
+
+        public int Blue { get; init; }
+
+        public RGB(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+}
